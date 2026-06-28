d30ce9d471511635498d7b50073c90d3bd40bd42
xiaohuai <3472304495@qq.com>
2026-06-28 18:02:07 +0800

    初始化

--- service/search/src/main/java/com/luvletter/search/SearchApplication.java
+++ service/search/src/main/java/com/luvletter/search/SearchApplication.java
@@ -1,11 +1,13 @@
 package com.luvletter.search;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
+@MapperScan("com.luvletter.search.mapper")
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.luvletter.search", "com.luvletter.model"})
 public class SearchApplication {
--- service/song/src/main/java/com/luvletter/song/SongApplication.java
+++ service/song/src/main/java/com/luvletter/song/SongApplication.java
@@ -1,10 +1,12 @@
 package com.luvletter.song;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@MapperScan("com.luvletter.song.mapper")
 @SpringBootApplication
 public class SongApplication {
 
